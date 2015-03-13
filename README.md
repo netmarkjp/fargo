9419ffff76ff924b74cfeebe2eb5491db7f0c3f2
Toshiaki Baba <toshiaki@netmark.jp>
2015-03-14 03:14:19 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,54 @@
+fargo
+==================
+
+fargo(Fast cARGO) is simple file copy daemon with HTTP.
+
+# Install
+
+```
+go get github.com/netmarkjp/fargo
+```
+
+# Usage
+
+## Start server
+
+```
+./fargo
+```
+
+## Push file to server
+
+1.get token
+
+```
+curl http://fargo.example.com:1236/token
+```
+
+2.push file
+
+```
+curl -F file=@somefile http://fargo.example.com:1236/push/<TOKEN>
+```
+
+## Get file from server
+
+```
+curl -O http://fargo.example.com:1236/get/<TOKEN>
+```
+
+# Specification
+
+- Listen address/port is ``0.0.0.0:1236`` by default.
+    - can change with env ``ADDR`` *feature*
+- username/password for ``/token`` is ``fargo`` / ``fargo`` by default. *feature*
+    - can change with env ``FARGO_USER`` and ``FARGO_PASSWORD`` *feature*
+- File store directory is ``/tmp`` by default.
+    - can change with env ``STORE_DIR`` *feature*
+- ``TOKEN`` is UUIDv4
+- ``TOKEN`` is expired in 5 min by default.
+    - can change with env ``TOKEN_TTL`` (min) *feature*
+- If get file fail, locked 30 seconds from same IP by default.
+- Pushed file will delete in 60 min by default.
+    - can change with env ``FILE_TTL`` (min) *feature*
+
---
+++ fargo.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"github.com/satori/go.uuid"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	config   *Config
+	tokens   *Tokens
+	failedIP *FailedIP
+)
+
+func init() {
+
+	config = &Config{
+		FargoUser:      "fargo",
+		FargoPassword:  "fargo",
+		StoreDirectory: "/tmp",
+		TokenTTL:       300,
+		FileTTL:        3600,
+	}
+
+	tokens = &Tokens{token: make(map[string]*Token)}
+	failedIP = &FailedIP{ip: make(map[string]time.Time)}
+}
+
+type Config struct {
+	FargoUser      string
+	FargoPassword  string
+	StoreDirectory string
+	TokenTTL       int64 //sec
+	FileTTL        int64 //sec
+	sync.Mutex
+}
+
+type Token struct {
+	ID        string //UUIDv4
+	Filename  string
+	CreatedAt time.Time
+	PushdAt   time.Time
+}
+
+type Tokens struct {
+	token map[string]*Token //Key: Token.ID, Value: Token
+	sync.Mutex
+}
+
+type FailedIP struct {
+	ip map[string]time.Time
+	sync.Mutex
+}
+
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	var newToken string
+	duplicated := true
+	for duplicated {
+		newToken = uuid.NewV4().String()
+		tokens.Lock()
+		if _, exist := tokens.token[newToken]; !exist {
+			tokens.token[newToken] = &Token{ID: newToken, CreatedAt: time.Now()}
+			duplicated = false
+		}
+		tokens.Unlock()
+	}
+	w.Write([]byte(newToken))
+}
+
+func pushHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	token := vars["token"]
+	log.Debug(token)
+	log.Debug(tokens.token)
+	if _, exist := tokens.token[token]; !exist {
+		log.Warn("token not found.", token)
+		w.WriteHeader(404)
+		return
+	}
+	if tokens.token[token].CreatedAt.Add(time.Duration(config.TokenTTL) * time.Second).Before(time.Now()) {
+		log.Warn("token expired.", token)
+		log.Debug("token:", tokens.token[token])
+		log.Debug("expired at:", tokens.token[token].CreatedAt.Add(time.Duration(config.TokenTTL)*time.Second))
+		w.WriteHeader(403)
+		return
+	}
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+	defer file.Close()
+
+	filepath := StoreFilePath(token)
+	out, err := os.Create(filepath)
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	tokens.token[token].Filename = header.Filename
+	tokens.token[token].PushdAt = time.Now()
+	w.WriteHeader(200)
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	tcpaddr, _ := net.ResolveTCPAddr("tcp", r.RemoteAddr)
+	addr := tcpaddr.IP.String()
+	if val, exist := failedIP.ip[addr]; exist {
+		if val.Add(30 * time.Second).After(time.Now()) {
+			log.Warn("access denied for failed IP.")
+			w.WriteHeader(403)
+			return
+		}
+	}
+
+	vars := mux.Vars(r)
+	token := vars["token"]
+
+	if _, exist := tokens.token[token]; !exist {
+		log.Warn("token not found.", token)
+		failedIP.Lock()
+		failedIP.ip[addr] = time.Now()
+		failedIP.Unlock()
+		log.Debug("failedIP:", failedIP)
+		w.WriteHeader(404)
+		return
+	}
+	if tokens.token[token].CreatedAt.Add(time.Duration(config.TokenTTL) * time.Second).Before(time.Now()) {
+		log.Warn("token expired.", token)
+		failedIP.Lock()
+		failedIP.ip[addr] = time.Now()
+		failedIP.Unlock()
+		w.WriteHeader(403)
+		return
+	}
+
+	failedIP.Lock()
+	delete(failedIP.ip, addr)
+	failedIP.Unlock()
+
+	filepath := StoreFilePath(token)
+
+	contentDescription := fmt.Sprintf("attachment; filename=\"%s\"", tokens.token[token].Filename)
+	w.Header().Add("Content-Disposition", contentDescription)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	http.ServeFile(w, r, filepath)
+
+}
+
+func fileGC() {
+	for {
+		for k := range tokens.token {
+			if tokens.token[k].PushdAt.Add(time.Duration(config.FileTTL) * time.Second).Before(time.Now()) {
+				log.Debug("file gc: clean up ", k)
+				filepath := StoreFilePath(k)
+				if err := os.Remove(filepath); err != nil {
+					log.Error("failed to remove file. ", err)
+				}
+				tokens.Lock()
+				delete(tokens.token, k)
+				tokens.Unlock()
+			}
+		}
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func StoreFilePath(token string) string {
+	filepath := fmt.Sprintf("%s%c%s",
+		config.StoreDirectory,
+		os.PathSeparator,
+		token)
+	return filepath
+}
+
+func main() {
+	var err error
+	log.SetOutput(os.Stderr)
+	log.SetLevel(log.InfoLevel)
+
+	var debug bool
+	flag.BoolVar(&debug, "d", false, "debug logging (default: false)")
+
+	flag.Parse()
+
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/token", tokenHandler).Methods("GET")
+	r.HandleFunc("/push/{token}", pushHandler).Methods("POST")
+	r.HandleFunc("/get/{token}", getHandler).Methods("GET")
+	http.Handle("/", r)
+
+	go fileGC()
+
+	bind := "0.0.0.0:1236"
+
+	log.Debug("http server started")
+	log.Debug("bind: ", bind)
+
+	err = http.ListenAndServe(bind, nil)
+	if err != nil {
+		log.Error(err)
+	}
+}
